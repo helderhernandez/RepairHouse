686f3d007206507c0fe0a7995ad5266b7460ed0b
Helder Josué Hernández Díaz <helder.hernandez@ct-america.com>
2020-05-12 21:23:58 -0600

    Se agrego el proyecto

---
+++ Temporals/MyTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RepairHouse.Temporals
+{
+    public class MyTest
+    {
+        public String helloWord()
+        {
+            return "Hello word";
+        }
+    }
+}
---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepairHouse.Temporals;
+
+namespace Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        MyTest myTest = new MyTest();
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Debug.WriteLine(myTest.helloWord());
+        }
+    }
+}
