6e6727b1a6083dae3cd5737c7d461d074906354d
Bethcolorado <56610103+Bethcolorado@users.noreply.github.com>
2020-05-15 02:07:31 -0500

    Agregue, vista reportes e inventarios

---
+++ Controllers/InventarioController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepairHouse.Controllers
+{
+    public class InventarioController : Controller
+    {
+        // GET: Inventario
+        public ActionResult ListaInventario()
+        {
+            return View();
+        }
+    }
+}
