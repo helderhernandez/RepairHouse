5380b96325f48330833e5173cb168664f9fea15f
Helder-Hernandez <josue10hd@gmail.com>
2020-05-18 17:14:06 -0600

    - Se agrego Helder controller y vista para entrenamientos

---
+++ Controllers/HelderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepairHouse.Controllers
+{
+    public class HelderController : Controller
+    {
+        // GET: Helder
+        public ActionResult Formulario()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult FormularioBasico(string email, int edad)
+        {
+            Debug.WriteLine(email + " - " + edad);
+            return Content("todo salio bien :D");
+        }
+    }
+}
