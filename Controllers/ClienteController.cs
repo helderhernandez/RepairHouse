d1ee7eba2e15cb2be5ac55f482cc02a8689bdf6b
Helder-Hernandez <josue10hd@gmail.com>
2020-05-20 01:13:31 -0600

    - Se agrego el Cliente Controller con sus respectivas vistas

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RepairHouse.Models;
+
+namespace RepairHouse.Controllers
+{
+    public class ClienteController : Controller
+    {
+        private casa_reparadoraEntities db = new casa_reparadoraEntities();
+
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var cliente = db.Cliente.Include(c => c.Municipio);
+            return View(cliente.ToList());
+        }
+
+        // GET: Cliente/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: Cliente/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdMunicipio = new SelectList(db.Municipio, "IdMunicipio", "Municipio1");
+            return View();
+        }
+
+        // POST: Cliente/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCliente,PrimerNombre,SegundoNombre,PrimerApellido,SegundoApellido,DeCasada,Email,Sexo,DUI,NIT,Telefono,IdMunicipio")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cliente.Add(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdMunicipio = new SelectList(db.Municipio, "IdMunicipio", "Municipio1", cliente.IdMunicipio);
+            return View(cliente);
+        }
+
+        // GET: Cliente/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdMunicipio = new SelectList(db.Municipio, "IdMunicipio", "Municipio1", cliente.IdMunicipio);
+            return View(cliente);
+        }
+
+        // POST: Cliente/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCliente,PrimerNombre,SegundoNombre,PrimerApellido,SegundoApellido,DeCasada,Email,Sexo,DUI,NIT,Telefono,IdMunicipio")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdMunicipio = new SelectList(db.Municipio, "IdMunicipio", "Municipio1", cliente.IdMunicipio);
+            return View(cliente);
+        }
+
+        // GET: Cliente/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Cliente/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cliente cliente = db.Cliente.Find(id);
+            db.Cliente.Remove(cliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
