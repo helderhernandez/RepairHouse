245082b39a3aac70595c4d2bc8515b5534ccfaef
Helder-Hernandez <josue10hd@gmail.com>
2020-05-24 18:28:24 -0600

    Se agrego el Cliente con SXAFFOLFING
    changelog:
    
            new file:   Controllers/ClientesController.cs
            modified:   RepairHouse.csproj
            new file:   Views/Clientes/Create.cshtml
            new file:   Views/Clientes/Delete.cshtml
            new file:   Views/Clientes/Details.cshtml
            new file:   Views/Clientes/Edit.cshtml
            new file:   Views/Clientes/Index.cshtml

---
+++ Controllers/ClientesController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RepairHouse.Models;
+
+namespace RepairHouse.Controllers
+{
+    public class ClientesController : Controller
+    {
+        private casa_reparadoraEntities db = new casa_reparadoraEntities();
+
+        // GET: Clientes
+        public ActionResult Index()
+        {
+            var cliente = db.Cliente.Include(c => c.Municipio).Include(c => c.Sexo).Include(c => c.Factura);
+            return View(cliente.ToList());
+        }
+
+        // GET: Clientes/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // GET: Clientes/Create
+        public ActionResult Create()
+        {
+            ViewBag.IdMunicipio = new SelectList(db.Municipio, "IdMunicipio", "Municipio1");
+            ViewBag.IdSexo = new SelectList(db.Sexo, "IdSexo", "Sexo1");
+            ViewBag.IdCliente = new SelectList(db.Factura, "IdFactura", "IdFactura");
+            return View();
+        }
+
+        // POST: Clientes/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IdCliente,PrimerNombre,SegundoNombre,PrimerApellido,SegundoApellido,DeCasada,IdSexo,Email,DUI,NIT,Telefono,IdMunicipio,Domicilio")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Cliente.Add(cliente);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.IdMunicipio = new SelectList(db.Municipio, "IdMunicipio", "Municipio1", cliente.IdMunicipio);
+            ViewBag.IdSexo = new SelectList(db.Sexo, "IdSexo", "Sexo1", cliente.IdSexo);
+            ViewBag.IdCliente = new SelectList(db.Factura, "IdFactura", "IdFactura", cliente.IdCliente);
+            return View(cliente);
+        }
+
+        // GET: Clientes/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.IdMunicipio = new SelectList(db.Municipio, "IdMunicipio", "Municipio1", cliente.IdMunicipio);
+            ViewBag.IdSexo = new SelectList(db.Sexo, "IdSexo", "Sexo1", cliente.IdSexo);
+            ViewBag.IdCliente = new SelectList(db.Factura, "IdFactura", "IdFactura", cliente.IdCliente);
+            return View(cliente);
+        }
+
+        // POST: Clientes/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IdCliente,PrimerNombre,SegundoNombre,PrimerApellido,SegundoApellido,DeCasada,IdSexo,Email,DUI,NIT,Telefono,IdMunicipio,Domicilio")] Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(cliente).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdMunicipio = new SelectList(db.Municipio, "IdMunicipio", "Municipio1", cliente.IdMunicipio);
+            ViewBag.IdSexo = new SelectList(db.Sexo, "IdSexo", "Sexo1", cliente.IdSexo);
+            ViewBag.IdCliente = new SelectList(db.Factura, "IdFactura", "IdFactura", cliente.IdCliente);
+            return View(cliente);
+        }
+
+        // GET: Clientes/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Cliente cliente = db.Cliente.Find(id);
+            if (cliente == null)
+            {
+                return HttpNotFound();
+            }
+            return View(cliente);
+        }
+
+        // POST: Clientes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Cliente cliente = db.Cliente.Find(id);
+            db.Cliente.Remove(cliente);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
