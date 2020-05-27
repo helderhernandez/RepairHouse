20c4dd50303af64a866a8e55b3dd6d3b0c32dc73
Helder-Hernandez <josue10hd@gmail.com>
2020-05-26 20:54:40 -0600

    Se renombreo Equipoes a Equipos

--- Controllers/EquiposController.cs
+++ Controllers/EquiposController.cs
@@ -10,7 +10,7 @@ using RepairHouse.Models;
 
 namespace RepairHouse.Controllers
 {
-    public class EquipoesController : Controller
+    public class EquiposController : Controller
     {
         private casa_reparadoraEntities db = new casa_reparadoraEntities();
 
