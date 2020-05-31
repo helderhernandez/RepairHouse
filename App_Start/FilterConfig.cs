04ee84ced9dda96edb3a050ef44f40d09ad1d0c2
Helder-Hernandez <josue10hd@gmail.com>
2020-05-30 18:47:01 -0600

    - Se agrego filtro para verificar Login y Logout

--- App_Start/FilterConfig.cs
+++ App_Start/FilterConfig.cs
@@ -8,6 +8,7 @@ namespace RepairHouse
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
+            filters.Add(new Filters.VerifySession());
         }
     }
 }
---
+++ Filters/VerifySession.cs
@@ -0,0 +1,38 @@
+﻿using RepairHouse.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RepairHouse.Filters
+{
+    public class VerifySession : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var currentUser =  HttpContext.Current.Session["CURRENT_SESSION"];
+
+            if(currentUser == null)
+            {
+                if(filterContext.Controller is LoginController == false)
+                {
+                    if (filterContext.Controller is HomeController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("~/Login/Index");
+                    }
+                }
+            }
+            else
+            {
+                if (filterContext.Controller is LoginController == true)
+                {
+                    filterContext.HttpContext.Response.Redirect("~/Home/Index");
+                }
+            }
+
+
+            base.OnActionExecuting(filterContext);
+        }
+    }
+}
